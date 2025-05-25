b0b8ab8c8ae284436985e4d9ca4da263eafd9e5a
feng.dev <feng@vitruvio.cl>
2025-05-25 10:49:58 -0400

    Create csswriter.go

---
+++ sandbox/draf2/gocsswriter/csswriter.go
@@ -0,0 +1,19 @@
+package gocsswriter
+
+import "net/http"
+
+// GenerateCssFromString generate css from a html string
+// given a html with utility classes generate the css
+func GenerateCssFromString(Source string) string {
+	return ""
+}
+
+// GenerateCssFromFile generate css from a html file
+// given a html file with utility classes generate the css and save it to a file
+func GenerateCssFromFile(sourcePath string, destPath string) error {
+	return nil
+}
+
+func fo(w http.ResponseWriter, r *http.Request)
+
+type CSSWriter struct{}
